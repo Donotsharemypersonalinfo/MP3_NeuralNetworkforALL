6899019c6cfb248ae3486280c1a9dabbf24ecbf6
Donotsharemypersonalinfo <54XZ828ZX@gmail.com>
2024-12-10 19:57:34 -0500

    Update data & add example

--- \data.py
+++ \data.py
@@ -2,3 +2,16 @@ from tensorflow.keras.datasets import cifar10
 
 # Load CIFAR-10 dataset
 (X_train, y_train), (X_test, y_test) = cifar10.load_data()
+
+
+
+
+# Display some images from the dataset
+fig, axes = plt.subplots(2, 5, figsize=(10, 5))
+for i, ax in enumerate(axes.flatten()):
+    ax.imshow(x_train[i])
+    ax.set_title(f'Label: {y_train[i][0]}')
+    ax.axis('off')
+
+plt.tight_layout()
+plt.show()
